da31d5d4f2391ac6c9617117a4a0713220a6784d
jimmyvo253 <votansang253@gmail.com>
2026-01-16 20:00:37 +0700

    hehe

--- app/src/main/java/com/example/androidproject/data/local/CardEntity.kt
+++ app/src/main/java/com/example/androidproject/data/local/CardEntity.kt
@@ -44,27 +44,3 @@ interface FlashCardDao {
     @Delete
     suspend fun delete(flashCard: FlashCard)
 }
-
-@Database(entities = [FlashCard::class], version = 1)
-abstract class FlashCardDatabase : RoomDatabase() {
-    abstract fun flashCardDao(): FlashCardDao
-
-    companion object {
-        @Volatile // Ensures visibility to all threads
-        private var INSTANCE: FlashCardDatabase? = null
-
-        fun getDatabase(context: Context): FlashCardDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext, // Use application context to prevent memory leaks
-                    FlashCardDatabase::class.java,
-                    "FlashCardDatabase"
-                ).build()
-                INSTANCE = instance
-                // return instance
-                instance
-            }
-        }
-    }
-
-}
